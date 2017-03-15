6dc87766c27451d756dcf65ec63f8c32a424b460
diawest82 <diallowest@gmail.com>
2017-03-15 12:54:22 -0500

    Update league_builder.py

--- league_builder.py
+++ league_builder.py
@@ -1,1 +1,10 @@
+import sys
+import os
 
+# create main method
+#import csv file
+#import information into formatable code
+#divide teams equally into three "sharks" "raptors" and "dragons"
+#divide experienced players into three teams equally.  There are 9
+#create a 'teams.txt' file theat includes name of team and each player on each team
+#in the list of teams include, team name on one line, each player with their experience and guardian names on a seperate line seperated by commas.
